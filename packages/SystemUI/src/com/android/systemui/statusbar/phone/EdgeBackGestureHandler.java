4a8712bcb8c6daddd59abee1e347d525b29010f8
Pranav Vashi <neobuddy89@gmail.com>
2020-03-28 01:03:52 +0530

    EdgeBackGestureHandler: Handle all exceptions with WindowManagerService
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic3d096b99be4beda5dee51bed0f8966dc4b2f582

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -294,7 +294,7 @@ public class EdgeBackGestureHandler implements DisplayListener {
                 WindowManagerGlobal.getWindowManagerService()
                         .unregisterSystemGestureExclusionListener(
                                 mGestureExclusionListener, mDisplayId);
-            } catch (RemoteException e) {
+            } catch (Exception e) {
                 Log.e(TAG, "Failed to unregister window manager callbacks", e);
             }
 
@@ -308,7 +308,7 @@ public class EdgeBackGestureHandler implements DisplayListener {
                 WindowManagerGlobal.getWindowManagerService()
                         .registerSystemGestureExclusionListener(
                                 mGestureExclusionListener, mDisplayId);
-            } catch (RemoteException e) {
+            } catch (Exception e) {
                 Log.e(TAG, "Failed to register window manager callbacks", e);
             }
 
